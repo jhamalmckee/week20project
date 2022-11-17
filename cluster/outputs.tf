998b2758ed076e1da1a130ad67a174c502a5c057
jhamalmckee <92033046+jhamalmckee@users.noreply.github.com>
2022-11-16 20:40:25 -0500

    Update outputs.tf

--- cluster/outputs.tf
+++ cluster/outputs.tf
@@ -2,18 +2,18 @@
 
 
 output "endpoint" {
-  value = aws_eks_cluster.krypt0-week22.endpoint
+  value = aws_eks_cluster.week20-week20.endpoint
 }
 
 output "kubeconfig-certificate-authority-data" {
-  value = aws_eks_cluster.krypt0-week22.certificate_authority[0].data
+  value = aws_eks_cluster.week20-week20.certificate_authority[0].data
 }
 output "cluster_id" {
-  value = aws_eks_cluster.krypt0-week22.id
+  value = aws_eks_cluster.week20-week20.id
 }
 output "cluster_endpoint" {
-  value = aws_eks_cluster.krypt0-week22.endpoint
+  value = aws_eks_cluster.week20-week20.endpoint
 }
 output "cluster_name" {
-  value = aws_eks_cluster.krypt0-week22.name
+  value = aws_eks_cluster.week20-week20.name
 }
