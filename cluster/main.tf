0e202101e9f6e935e6c07acb852f412e503b900c
jhamalmckee <92033046+jhamalmckee@users.noreply.github.com>
2022-11-16 20:39:46 -0500

    Update main.tf

--- cluster/main.tf
+++ cluster/main.tf
@@ -5,33 +5,33 @@ resource "random_string" "random" {
   special = false
 }
 
-resource "aws_eks_cluster" "krypt0-week22" {
-  name     = "krypt0-week22-${random_string.random.result}"
-  role_arn = aws_iam_role.krypt0-week22.arn
+resource "aws_eks_cluster" "week20-week20" {
+  name     = "week20-week20-${random_string.random.result}"
+  role_arn = aws_iam_role.week20-week20.arn
 
   vpc_config {
     subnet_ids              = var.public_subnets
     endpoint_public_access  = var.endpoint_public_access
     endpoint_private_access = var.endpoint_private_access
-    security_group_ids      = [aws_security_group.krypt0-week22-node-group.id]
+    security_group_ids      = [aws_security_group.week20-week20-node-group.id]
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.krypt0-week22-AmazonEKSClusterPolicy,
-    aws_iam_role_policy_attachment.krypt0-week22-AmazonEKSVPCResourceController,
+    aws_iam_role_policy_attachment.week20-week20-AmazonEKSClusterPolicy,
+    aws_iam_role_policy_attachment.week20-week20-AmazonEKSVPCResourceController,
   ]
 }
 
 
 
-resource "aws_eks_node_group" "krypt0-week22" {
-  cluster_name   = aws_eks_cluster.krypt0-week22.name
-  node_role_arn  = aws_iam_role.krypt0-week22-cluster.arn
+resource "aws_eks_node_group" "week20-week20" {
+  cluster_name   = aws_eks_cluster.week20-week20.name
+  node_role_arn  = aws_iam_role.week20-week20-cluster.arn
   subnet_ids     = var.public_subnets[*]
   instance_types = var.instance_types
 
   remote_access {
-    source_security_group_ids = [aws_security_group.krypt0-week22-node-group.id]
+    source_security_group_ids = [aws_security_group.week20-week20-node-group.id]
     ec2_ssh_key               = var.key_pair
   }
 
@@ -42,15 +42,15 @@ resource "aws_eks_node_group" "krypt0-week22" {
   }
 
   depends_on = [
-    aws_iam_role_policy_attachment.krypt0-week22-AmazonEKSWorkerNodePolicy,
-    aws_iam_role_policy_attachment.krypt0-week22-AmazonEKS_CNI_Policy,
-    aws_iam_role_policy_attachment.krypt0-week22-AmazonEC2ContainerRegistryReadOnly,
+    aws_iam_role_policy_attachment.week20-week20-AmazonEKSWorkerNodePolicy,
+    aws_iam_role_policy_attachment.week20-week20-AmazonEKS_CNI_Policy,
+    aws_iam_role_policy_attachment.week20-week20-AmazonEC2ContainerRegistryReadOnly,
   ]
 }
 
 
-resource "aws_security_group" "krypt0-week22-node-group" {
-  name_prefix = "krypt0-week22-node-group"
+resource "aws_security_group" "week20-week20-node-group" {
+  name_prefix = "week20-week20-node-group"
   vpc_id      = var.vpc_id
 
   ingress {
@@ -69,8 +69,8 @@ resource "aws_security_group" "krypt0-week22-node-group" {
 }
 
 
-resource "aws_iam_role" "krypt0-week22" {
-  name = "krypt0-week22-eks-cluster"
+resource "aws_iam_role" "week20-week20" {
+  name = "week20-week20-eks-cluster"
 
   assume_role_policy = jsonencode({
 
@@ -85,8 +85,8 @@ resource "aws_iam_role" "krypt0-week22" {
   })
 }
 
-resource "aws_iam_role" "krypt0-week22-cluster" {
-  name = "krypt0-week22-node-group"
+resource "aws_iam_role" "week20-week20-cluster" {
+  name = "week20-week20-node-group"
 
   assume_role_policy = jsonencode({
     Statement = [{
@@ -100,27 +100,27 @@ resource "aws_iam_role" "krypt0-week22-cluster" {
   })
 }
 
-resource "aws_iam_role_policy_attachment" "krypt0-week22-AmazonEKSVPCResourceController" {
+resource "aws_iam_role_policy_attachment" "week20-week20-AmazonEKSVPCResourceController" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSVPCResourceController"
-  role       = aws_iam_role.krypt0-week22.name
+  role       = aws_iam_role.week20-week20.name
 }
 
 resource "aws_iam_role_policy_attachment" "krypt0-week22-AmazonEKSWorkerNodePolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
-  role       = aws_iam_role.krypt0-week22-cluster.name
+  role       = aws_iam_role.week20-week20-cluster.name
 }
 
 resource "aws_iam_role_policy_attachment" "krypt0-week22-AmazonEKS_CNI_Policy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKS_CNI_Policy"
-  role       = aws_iam_role.krypt0-week22-cluster.name
+  role       = aws_iam_role.week20-week20-cluster.name
 }
 
 resource "aws_iam_role_policy_attachment" "krypt0-week22-AmazonEC2ContainerRegistryReadOnly" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEC2ContainerRegistryReadOnly"
-  role       = aws_iam_role.krypt0-week22-cluster.name
+  role       = aws_iam_role.week20-week20-cluster.name
 }
 
 resource "aws_iam_role_policy_attachment" "krypt0-week22-AmazonEKSClusterPolicy" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonEKSClusterPolicy"
-  role       = aws_iam_role.krypt0-week22.name
+  role       = aws_iam_role.week20-week20.name
 }
